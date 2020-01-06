d75a9a768d831e1be28015faf944abd9273ed8d2
Troy Kollar <troykollar@gmail.com>
2020-01-06 13:53:34 -0500

    Added feature_homography

---
+++ feature_homography/featureHomography.py
@@ -0,0 +1,42 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+MIN_MATCH_COUNT = 10
+
+img1 = cv2.imread('box.png', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread('box_in_scene.png', cv2.IMREAD_GRAYSCALE)
+
+
+
+#ORB detector
+orb = cv2.ORB_create()
+
+kp1, des1 = orb.detectAndCompute(img1, None)
+kp2, des2 = orb.detectAndCompute(img2, None)
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING2, crossCheck=True)
+
+matches = bf.match(des1, des2)
+matches = sorted(matches, key= lambda x:x.distance)
+
+if len(matches) > MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h,w = img1.shape[:2]
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+
+else:
+    print("Not enough matches".format(len(matches), MIN_MATCH_COUNT))
+    print(len(matches))
+    matchesMask = None
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches,None)
+plt.imshow(img3, 'gray'),plt.show()
