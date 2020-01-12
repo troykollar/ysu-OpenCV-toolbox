65182e4cee93904348a0ee5f7bba8cbdf718962c
Troy Kollar <troykollar@gmail.com>
2020-01-12 14:07:43 -0500

    Improved mouseMask for multiple rectangles

--- mask_from_mouse/mouseMask.py
+++ mask_from_mouse/mouseMask.py
@@ -3,27 +3,39 @@ import numpy as np
 import csv
 import sys
 
-outFilename = sys.argv[1]
+if sys.argv[1] == None:
+    noOutput = True
+else:
+    noOutput = False
+
+if noOutput == False:
+    outFilename = sys.argv[1]
+
 refPoint = []       # List for each corner of the rectangle
-dragging = False    # Says whether or not the user is currently dragging the box
+lButtonDrag = False    # Says whether or not the user is currently dragging lbutton
+rButtonDrag = False    # Says whether or not the user is currently dragging rbutton
 
 def onClick(event, x, y, flags, param):
     #Grab global variables
-    global refPoint, dragging, mask, image, image_copy
+    global refPoint, lButtonDrag, rButtonDrag, mask, mask_copy, image, image_copy
     if event == cv2.EVENT_LBUTTONDOWN:  # Once click is started
         refPoint.append((x,y))          # Append first point of the rectangle
-        dragging = True                 # Indicate drawing has started
+        lButtonDrag = True                 # Indicate drawing has started
 
-    if (dragging == True):                  # while the user is dragging
+    if (lButtonDrag == True):               # while the user is dragging lButton
         if (event == cv2.EVENT_LBUTTONUP):  # if user releases
             refPoint.append((x,y))          # append the second rectangle corner
-            dragging = False                # Indicate the user is no longer dragging
+            mask_copy = mask.copy()
+            image_copy = image.copy()
+            lButtonDrag = False                # Indicate the user is no longer dragging lButton
 
             #Draw rectangle
             cv2.rectangle(image, refPoint[0], refPoint[1], (0, 255, 0), 2)
             cv2.rectangle(mask, refPoint[0], refPoint[1], (1), 0)
 
-        if (event == cv2.EVENT_MOUSEMOVE):  # While user is dragging, draw rectangle
+            refPoint.clear()    # Clear reference points so new rectangles can be drawn
+
+        if (event == cv2.EVENT_MOUSEMOVE):  # While user is lButtonDrag, draw rectangle
             image = image_copy.copy()       # Revert to original image each mouse move, so rectangles don't overlap
             mask = mask_copy.copy()         # Revert to original mask each mouse move, so rectangles don't overlap
 
@@ -51,10 +63,11 @@ while True:
     if key == ord('r'):             # If user presses r
         image = image_copy.copy()   # Revert to original copy of image
         refPoint.clear()            # Clear rectangle corners
-        dragging = False            # Indicate the user is not dragging
+        lButtonDrag = False            # Indicate the user is not lButtonDrag
     elif key == 27:     # 27 represents escape key 
         break
 
-writeToCSV(mask, outFilename)
+if noOutput == False:
+    writeToCSV(mask, outFilename)
 
 cv2.destroyAllWindows()
