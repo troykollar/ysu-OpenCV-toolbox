eca54288d0ee592f1ee3e737c8e4c0c712aa8549
troy <troykollar@gmail.com>
2020-02-05 13:56:30 -0500

    Formatted and added DogBone images

--- bf_feature_matching/featureMatching.py
+++ bf_feature_matching/featureMatching.py
@@ -2,20 +2,27 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img1 = cv2.imread('avengers.jpg',cv2.IMREAD_GRAYSCALE)
-img2 = cv2.imread('avengers_on_desk.jpg',cv2.IMREAD_GRAYSCALE)
+img1 = cv2.imread('DogBone3-1.jpg', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread('Overmold1-1.jpg', cv2.IMREAD_GRAYSCALE)
 
 orb = cv2.ORB_create()
 
-kp1, des1 = orb.detectAndCompute(img1,None)
-kp2, des2 = orb.detectAndCompute(img2,None)
+kp1, des1 = orb.detectAndCompute(img1, None)
+kp2, des2 = orb.detectAndCompute(img2, None)
 
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
 matches = bf.match(des1, des2)
 
-matches = sorted(matches, key = lambda x:x.distance)
+matches = sorted(matches, key=lambda x: x.distance)
 
-img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:25], None, flags=cv2.DrawMatchesFlags_DEFAULT)
+img3 = cv2.drawMatches(img1,
+                       kp1,
+                       img2,
+                       kp2,
+                       matches,
+                       None,
+                       flags=cv2.DrawMatchesFlags_DEFAULT)
 
-plt.imshow(img3), plt.show()
+plt.imshow(img3)
+plt.show()
