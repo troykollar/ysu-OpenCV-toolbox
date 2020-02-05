720a8b79c0b76e71a128740c70df5b610718c0a6
Troy Kollar <troykollar@gmail.com>
2020-02-05 09:15:30 -0500

    Improved formatting and added various docstrings

--- np_vid_viewer/np_vid_viewer.py
+++ np_vid_viewer/np_vid_viewer.py
@@ -3,6 +3,7 @@ import numpy as np
 import datetime
 from reflection_remover import ReflectionRemover
 
+
 class NpVidViewer:
     """NpVidViewer
     Stores information relating to a video to be able to easily display a video from a numpy array
@@ -26,8 +27,13 @@ class NpVidViewer:
     _lower_bounds
         List of lower bounds of part. Used to remove the bottom reflection.
     """
-    def __init__(self, filename: str, window_name="Video", melt_pool_data, tc_times=None,
-                 remove_reflection=False, remove_lower=False):
+    def __init__(self,
+                 filename: str,
+                 melt_pool_data,
+                 tc_times,
+                 window_name="Video",
+                 remove_reflection=False,
+                 remove_lower=False):
         """Create an NpVidViewer Object.
 
         Parameters
@@ -46,8 +52,8 @@ class NpVidViewer:
             Run the remove_lower reflection function if true.
         """
         self._remove_reflection = remove_reflection
-        self.remove_lower = remove_lower
-        self._array = np.load(filename, mmap_mode='r', allow_pickle=True)
+        self._remove_lower = remove_lower
+        self._array = np.load(filename, mmap_mode="r", allow_pickle=True)
         self._speed = 1
         self._window_name = window_name
         self._num_frames = self.array.shape[0]
@@ -62,30 +68,39 @@ class NpVidViewer:
         self._lower_bounds = self.find_lower_bounds()
 
     def find_lower_bounds(self):
-        """Set docstring here.
-        
+        """Find the lower bounds of the piece.
+
         Returns
         -------
-
-        """   
-        #Compares x coordinates of current frame max and next frame max
+        max_locations
+            List of the first points at the zero level below the each max temp.
+        """
         img_array = self.array
         i = 0
+        # Find the x and y value of the max temp
         max_x = np.where(img_array[0] == np.amax(img_array[0]))[1][0]
-        next_max_x = np.where(img_array[1] == np.amax(img_array[1]))[1][0]
         max_y = np.where(img_array[0] == np.amax(img_array[0]))[0][0]
+
+        # Find the x value of the max temp of the next frame
+        next_max_x = np.where(img_array[1] == np.amax(img_array[1]))[1][0]
+
+        # Create lists of the x and y values of the max temperatures
         max_x_locations = []
         max_y_locations = []
-        while (max_x < next_max_x):
+
+        # While the laser is moving to the right. (i.e. the next location > current location)
+        while max_x < next_max_x:
             max_x_locations.append(max_x)
             max_y_locations.append(max_y)
-            i = i+1
+            i = i + 1
             max_x = np.where(img_array[i] == np.amax(img_array[i]))[1][0]
-            next_max_x = np.where(img_array[i + 1] == np.amax(img_array[i + 1]))[1][0]
+            next_max_x = np.where(
+                img_array[i + 1] == np.amax(img_array[i + 1]))[1][0]
             max_y = np.where(img_array[i] == np.amax(img_array[i]))[0][0]
+
         max_locations = []
         j = 0
-        for i in range (max_x_locations[0], max_x_locations[-1]):
+        for i in range(max_x_locations[0], max_x_locations[-1]):
             if i > max_x_locations[j]:
                 j = j + 1
             max_locations.append((i, max_y_locations[j]))
@@ -93,72 +108,101 @@ class NpVidViewer:
 
     @property
     def lower_bounds(self):
+        """Return the lower bounds"""
         return self._lower_bounds
 
     def match_vid_to_meltpool(self):
+        """Shuffle the meltpool data and thermal camera data together based on timestamp"""
         self._matched_array = []
         for i in range(0, self.array.shape[0]):
             if self.mp_data_index + 1 < self.melt_pool_data.shape[0]:
-                if self.timestamps[i] >= self.melt_pool_data[self.mp_data_index + 1][0]:
+                if self.timestamps[i] >= self.melt_pool_data[self.mp_data_index
+                                                             + 1][0]:
                     self.mp_data_index = self.mp_data_index + 1
-            self._matched_array.append([i, self.timestamps[i], self.melt_pool_data[self.mp_data_index][0],
-                                        self.melt_pool_data[self.mp_data_index][1],
-                                        self.melt_pool_data[self.mp_data_index][2],
-                                        self.melt_pool_data[self.mp_data_index][3],
-                                        self.melt_pool_data[self.mp_data_index][4]])
+            self._matched_array.append([
+                i, self.timestamps[i],
+                self.melt_pool_data[self.mp_data_index][0],
+                self.melt_pool_data[self.mp_data_index][1],
+                self.melt_pool_data[self.mp_data_index][2],
+                self.melt_pool_data[self.mp_data_index][3],
+                self.melt_pool_data[self.mp_data_index][4]
+            ])
 
     def save_video(self, filename="Video.avi", framerate=60):
+        """Generate video based on `_array` information, and save under `filename`.
+
+        Parameters
+        ----------
+        filename : str
+            Name to save the video file as.
+        framerate : int
+            Framerate to generate the video with.
+        """
         imgs = []
         height = self.array[0].shape[0]
         width = self.array[0].shape[1]
         size = (width, height)
-        out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc('f','m','p','4'), framerate, size)
+        out = cv2.VideoWriter(filename,
+                              cv2.VideoWriter_fourcc("f", "m", "p", "4"),
+                              framerate, size)
         for i in range(0, self.array.shape[0]):
-            percent = (i/self.array.shape[0])*100
+            percent = (i / self.array.shape[0]) * 100
             print("Saving video: " + str("%.2f" % percent) + "%")
             img = self.array[i]
             normalized_img = img.copy()
             if self.remove_reflection:
-                ReflectionRemover.remove(normalized_img, zero_level_threshold=180, max_temp_threshold=700)
+                ReflectionRemover.remove(normalized_img,
+                                         zero_level_threshold=180,
+                                         max_temp_threshold=700)
 
-            normalized_img = cv2.normalize(normalized_img, normalized_img, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-            normalized_img = cv2.applyColorMap(normalized_img, cv2.COLORMAP_INFERNO)
+            normalized_img = cv2.normalize(normalized_img, normalized_img, 0,
+                                           255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+            normalized_img = cv2.applyColorMap(normalized_img,
+                                               cv2.COLORMAP_INFERNO)
 
             self.add_mp_data_to_img(normalized_img, i)
-            
+
             out.write(normalized_img)
-        
+
         out.release
-            
 
     @property
     def remove_reflection(self):
+        """Return `_remove_reflection` attribute"""
         return self._remove_reflection
 
     @property
     def num_frames(self):
+        """Return `_num_frames` attritbute. The number of frames in the video."""
         return self._num_frames
 
     def video_timestamp(self, frame):
+        """Return the timestamp of the video based on the frame."""
         return self.matched_array[frame][1]
 
     def mp_timestamp(self, frame):
+        """Return the timestamp of the melt pool data based on the frame."""
         return self.matched_array[frame][2]
 
     def mp_x(self, frame):
+        """Return the meltpool x value based on the frame"""
         return self.matched_array[frame][3]
 
     def mp_y(self, frame):
+        """Return the meltpool y value based on the frame"""
         return self.matched_array[frame][4]
 
     def mp_z(self, frame):
+        """Return the meltpool z value based on the frame"""
         return self.matched_array[frame][5]
 
     def mp_area(self, frame):
+        """Return the meltpool area value based on the frame"""
         return self.matched_array[frame][6]
 
     @property
     def matched_array(self):
+        """Return the array of meltpool and video matched values"""
         return self._matched_array
 
     @property
@@ -177,10 +221,6 @@ class NpVidViewer:
     @property
     def melt_pool_data(self):
         return self._melt_pool_data
-
-    @property
-    def num_frames(self):
-        return self._num_frames
 
     @property
     def window_name(self):
@@ -208,44 +248,77 @@ class NpVidViewer:
         return self._timestamps
 
     def print_info(self, frame):
-        print("Frame: " + str(frame),
-              "| TC time: " + str(self.mp_timestamp(frame).replace(microsecond = 0)),
-              "| MP time: " + str(self.mp_timestamp(frame).replace(microsecond = 0)),
-              "| MP X: " + str(self.mp_x(frame)), 
-              "| MP Y: " + str(self.mp_y(frame)), 
-              "| MP Z: " + str(self.mp_z(frame)),
-              "| MP Area: " + str(self.mp_area(frame)))
+        print(
+            "Frame: " + str(frame),
+            "| TC time: " +
+            str(self.mp_timestamp(frame).replace(microsecond=0)),
+            "| MP time: " +
+            str(self.mp_timestamp(frame).replace(microsecond=0)),
+            "| MP X: " + str(self.mp_x(frame)),
+            "| MP Y: " + str(self.mp_y(frame)),
+            "| MP Z: " + str(self.mp_z(frame)),
+            "| MP Area: " + str(self.mp_area(frame)),
+        )
 
     def update_image(self, frame: int):
         img = self.array[frame]
         normalized_img = img.copy()
         if self.remove_reflection:
-            ReflectionRemover.remove(normalized_img, zero_level_threshold=180,
-                                     max_temp_threshold=700, remove_lower=True,
-                                     lower_bounds=self.lower_bounds)
+            ReflectionRemover.remove(
+                normalized_img,
+                zero_level_threshold=180,
+                max_temp_threshold=700,
+                remove_lower=True,
+                lower_bounds=self.lower_bounds,
+            )
 
-        normalized_img = cv2.normalize(normalized_img, normalized_img, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-        normalized_img = cv2.applyColorMap(normalized_img, cv2.COLORMAP_INFERNO)
+        normalized_img = cv2.normalize(normalized_img, normalized_img, 0, 255,
+                                       cv2.NORM_MINMAX, cv2.CV_8UC1)
+        normalized_img = cv2.applyColorMap(normalized_img,
+                                           cv2.COLORMAP_INFERNO)
 
         self.add_mp_data_to_img(normalized_img, frame)
 
         self.print_info(frame)
-        cv2.imshow(self.window_name, normalized_img)
+        return normalized_img
 
     def add_mp_data_to_img(self, img, frame):
         img_height = img.shape[:1][0]
         font = cv2.FONT_HERSHEY_DUPLEX
-        font_size = img_height/480
+        font_size = img_height / 480
         font_color = (255, 255, 255)
-        img = cv2.putText(img, "X: " + str(self.mp_x(frame)),
-                          (50, int((1/16)*img_height)), font, font_size, font_color)
-        img = cv2.putText(img, "Y: " + str(self.mp_y(frame)),
-                          (50, int((2/16)*img_height)), font, font_size, font_color)
-        img = cv2.putText(img, "Z: " + str(self.mp_z(frame)), 
-                          (50, int((3/16)*img_height)), font, font_size, font_color)
-        img = cv2.putText(img, "Area: " + str(self.mp_area(frame)),
-                          (50, int((4/16)*img_height)), font, font_size, font_color)
-
+        img = cv2.putText(
+            img,
+            "X: " + str(self.mp_x(frame)),
+            (50, int((1 / 16) * img_height)),
+            font,
+            font_size,
+            font_color,
+        )
+        img = cv2.putText(
+            img,
+            "Y: " + str(self.mp_y(frame)),
+            (50, int((2 / 16) * img_height)),
+            font,
+            font_size,
+            font_color,
+        )
+        img = cv2.putText(
+            img,
+            "Z: " + str(self.mp_z(frame)),
+            (50, int((3 / 16) * img_height)),
+            font,
+            font_size,
+            font_color,
+        )
+        img = cv2.putText(
+            img,
+            "Area: " + str(self.mp_area(frame)),
+            (50, int((4 / 16) * img_height)),
+            font,
+            font_size,
+            font_color,
+        )
 
     def play_video(self, speed=1):
         cv2.namedWindow(self.window_name, cv2.WINDOW_NORMAL)
@@ -256,25 +329,33 @@ class NpVidViewer:
         while True:
             key = cv2.waitKey(self.speed)
             if not pause:
-                self.update_image(frame)
+                img = self.update_image(frame)
+                cv2.imshow(self.window_name, img)
                 frame = frame + 1
             elif pause:
-                if key == ord('s'):
-                    np.savetxt('tc_temps-' + str(i + 1) + ".csv", img, fmt='%d', delimiter=",")
-                elif key == ord('l'):
+                if key == ord("s"):
+                    np.savetxt(
+                        "tc_temps-" + str(frame + 1) + ".csv",
+                        img,
+                        fmt="%d",
+                        delimiter=",",
+                    )
+                elif key == ord("l"):
                     frame = frame + 10
-                    self.update_image(frame)
-                elif key == ord('j'):
+                    img = self.update_image(frame)
+                    cv2.imshow(self.window_name, img)
+                elif key == ord("j"):
                     if frame > 10:
                         frame = frame - 10
                     else:
                         frame = 0
-                    self.update_image(frame)
-                    
-            if key == ord('q'):
+                    img = self.update_image(frame)
+                    cv2.imshow(self.window_name, img)
+
+            if key == ord("q"):
                 break
-            elif key == ord('k'):
+            elif key == ord("k"):
                 pause = not pause
-            elif  frame >= self.num_frames:
+            elif frame >= self.num_frames:
                 break
         cv2.destroyAllWindows
